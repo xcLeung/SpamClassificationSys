c071efccefb58b0a44067dc51600cf9f71490772
xcleung <xcleung0221@gmail.com>
2014-02-09 23:20:42 +0800

    分别获取邮件正文与主题

--- src/feature_extraction/extraction.java
+++ src/feature_extraction/extraction.java
@@ -5,9 +5,14 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.python.antlr.PythonParser.return_stmt_return;
+
 public class extraction {
 	
 	private ArrayList<String> m_Content=new ArrayList<String>();
+	private String m_Code;
+	private String m_Subject;
 	
 	private void GetFileContent(String filePath){
 		File resfile = new File(filePath);
@@ -17,6 +22,15 @@ public class extraction {
 				Scanner input = new Scanner(resfile);
 				while(input.hasNextLine()){
 					String str=input.nextLine();
+					if(str.contains("主题")){
+						String[] result=str.split(":");
+						if(result.length==2){
+							m_Subject=result[1];
+						}else{
+							m_Subject="";
+						}
+						System.out.println(m_Subject);
+					}
 					m_Content.add(str);
 				}
 			} catch (FileNotFoundException e) {
@@ -27,13 +41,49 @@ public class extraction {
 			return ;
 	}
 	
+	/***
+	 * 邮件头特征
+	 * @return
+	 */
+	private String GetSubjectCode(){
+		String res="";
+		return res;
+	}
+	
+	/***
+	 * 邮件体特征
+	 * @return
+	 */
+	private String GetContentCode(){
+		String res="";
+		return res;
+	}
+	
+	/***
+	 * 获取特征串
+	 * @return
+	 */
+	private String GetAllCode(){
+		String str="";
+		str+=GetSubjectCode();
+		str+=GetContentCode();
+		return str;
+	}
+	
+	
+	/***
+	 * 特征提取入口函数
+	 * @param filePath
+	 * @return
+	 */
 	public String feature_extraction(String filePath){
 		GetFileContent(filePath);
 		System.out.println("正文内容：");
 		for(String str:m_Content){
 			System.out.println(str);
 		}
-		return  "lxc";
+		m_Code=GetAllCode();
+		return  m_Code;
 	}
 	
 	/***
