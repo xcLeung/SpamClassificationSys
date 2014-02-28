aa49e7c91854a5ab8ce06dbc4359512db8616826
xcleung <xcleung0221@gmail.com>
2014-02-28 16:13:06 +0800

    处理分词结果(未完成)

--- src/divide_word/DivideWord.java
+++ src/divide_word/DivideWord.java
@@ -6,84 +6,52 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.python.antlr.PythonParser.return_stmt_return;
+
 import ICTCLAS.I3S.AC.*;
 import feature_extraction.extraction;
 
-public class DivideWord {
+public class DivideWord extends WordResult{
 
-	private String m_FilePath="";
+	private String m_FilePath1="";
+	private int m_FileSum1=0;
+	private String m_FilePath2="";
+	private int m_FileSum2=0;
+	
+	private int m_ClassSum=0;
+	private int m_FilesSum=0;
+	
 	private ArrayList<String> m_MailsContent=new ArrayList<String>();
+	
+	private ArrayList<WordResult> m_WordResults=new ArrayList<WordResult>();
 	
 	private Map<String, Integer> m_WordAllMap = new HashMap<String,Integer>();
 	
 	private static int m_WordSum=0;
 	
-	public DivideWord(String sFilePath){
-		m_FilePath=sFilePath;
+	public DivideWord(File[] files){
+		m_ClassSum=files.length;
+		for(File file:files){
+			WordResult oWordResult=new WordResult();
+			oWordResult.SetFileDict(file);
+			m_FilesSum+=oWordResult.GetFilesSum();
+			m_WordResults.add(oWordResult);
+		}
 	}
 	
-	/***
-	 * 对字符串分词(中科院分词)
-	 */
-	private String ICTCLASStringProcess(String sInput){	
-		try {
-			ICTCLAS50 oICTCLAS50 = new ICTCLAS50();
-			String argu = ".";
-			//初始化
-			if (oICTCLAS50.ICTCLAS_Init(argu.getBytes("GB2312")) == false)
-			{
-				System.out.println("Init Fail!");
-				return "";
-			}
-			//设置词性标注集(0 计算所二级标注集，1 计算所一级标注集，2 北大二级标注集，3 北大一级标注集)
-			oICTCLAS50.ICTCLAS_SetPOSmap(2);
-			//导入用户词典前分词
-			byte nativeBytes[] = oICTCLAS50.ICTCLAS_ParagraphProcess(sInput.getBytes("GB2312"), 0, 0);//分词处理
-			System.out.println(nativeBytes.length);
-			String nativeStr = new String(nativeBytes, 0, nativeBytes.length, "GB2312");
-			System.out.println("分词结果： " + nativeStr);//打印结果
-			return nativeStr;
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "";
+	private File GetFileInfo(String filepath,int index){
+		File fDictFile=new File(filepath);
+		return fDictFile;
 	}
 	
 	/***
 	 * 获取邮件正文内容，分词处理，统计
 	 */
-	private void DealWords(){
-		File fDictFile=new File(m_FilePath);
-		File[] files=fDictFile.listFiles();
-		extraction oExtraction=new extraction();
-		String sContent="";
-		String sResult="";
-		String[] ssWord;
-		for(File file:files){
-			oExtraction.Release();
-			sContent="";
-			sResult="";
-			sContent=oExtraction.GetEmailContent(file.getAbsolutePath());
-			sContent=sContent.substring(0, sContent.length()/2);
-			m_MailsContent.add(sContent);
-			if(sContent.length()>0)
-				sResult=ICTCLASStringProcess(sContent);
-			ssWord=sResult.split(" ");
-			if(ssWord.length>0)
-				m_WordSum+=ssWord.length;
-			for(int i=0;i<ssWord.length;i++){
-				Integer oValue=m_WordAllMap.get(ssWord[i]);
-				if(oValue!=null)
-					m_WordAllMap.put(ssWord[i], oValue+1);
-				else
-					m_WordAllMap.put(ssWord[i], 1);
-			}
-		}
+	public void DealWords(){
 		
-		for(String key:m_WordAllMap.keySet()){
-			System.out.println(key+":"+(m_WordAllMap.get(key)*1./m_WordSum));
-		}
+	//	for(String key:m_WordAllMap.keySet()){
+	//		System.out.println(key+":"+(m_WordAllMap.get(key)*1./m_WordSum));
+	//	}
 	}
 	
 	/***
@@ -101,8 +69,6 @@ public class DivideWord {
 		// TODO Auto-generated method stub
 		String filePath = "F:\\MailProject\\梁祥超-毕业设计\\emailtest3-decode";
 		System.out.println("中文分词文件夹："+filePath);
-		DivideWord oDivideWord=new DivideWord(filePath);
-		oDivideWord.DealWords();
 	}
 
 }
---
+++ src/divide_word/WordResult.java
@@ -0,0 +1,90 @@
+package divide_word;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.python.antlr.PythonParser.return_stmt_return;
+
+import ICTCLAS.I3S.AC.ICTCLAS50;
+
+import feature_extraction.extraction;
+
+public class WordResult {
+	
+	private File m_FileDict;
+	private File[] m_Files;
+	private int m_FilesSum=0;
+	private ArrayList<String> m_Words=new ArrayList<String>();
+	private Map<String, Integer> m_WordAllMap = new HashMap<String,Integer>();
+	
+	public WordResult(){
+		
+	}
+	
+	/***
+	 * 获得本类别文档总数
+	 * @return
+	 */
+	public int GetFilesSum(){
+		return m_FilesSum;
+	}
+	
+	public void SetFileDict(File file){
+		m_FileDict=file;
+		m_Files=m_FileDict.listFiles();
+		m_FilesSum=m_Files.length;
+	}
+	
+	public void SetFilesWord(){
+		extraction oExtraction=new extraction();
+		String sContent="";
+		String sResult="";
+		String[] ssWord;
+		for(File file:m_Files){
+			oExtraction.Release();
+			sContent=oExtraction.GetEmailContent(file.getAbsolutePath());
+			sContent=sContent.substring(0, sContent.length()/2);
+			if(sContent.length()>0)
+				sResult=ICTCLASStringProcess(sContent);
+			ssWord=sResult.split(" ");
+			for(int i=0;i<ssWord.length;i++){
+				Integer oValue=m_WordAllMap.get(ssWord[i]);
+				if(oValue!=null)
+					m_WordAllMap.put(ssWord[i], oValue+1);
+				else
+					m_WordAllMap.put(ssWord[i], 1);
+			}
+		}	
+	}
+	
+	/***
+	 * 对字符串分词(中科院分词)
+	 */
+	private String ICTCLASStringProcess(String sInput){	
+		try {
+			ICTCLAS50 oICTCLAS50 = new ICTCLAS50();
+			String argu = ".";
+			//初始化
+			if (oICTCLAS50.ICTCLAS_Init(argu.getBytes("GB2312")) == false)
+			{
+				System.out.println("Init Fail!");
+				return "";
+			}
+			//设置词性标注集(0 计算所二级标注集，1 计算所一级标注集，2 北大二级标注集，3 北大一级标注集)
+			oICTCLAS50.ICTCLAS_SetPOSmap(2);
+			//导入用户词典前分词
+			byte nativeBytes[] = oICTCLAS50.ICTCLAS_ParagraphProcess(sInput.getBytes("GB2312"), 0, 0);//分词处理
+			System.out.println(nativeBytes.length);
+			String nativeStr = new String(nativeBytes, 0, nativeBytes.length, "GB2312");
+			//System.out.println("分词结果： " + nativeStr);//打印结果
+			return nativeStr;
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
+}
